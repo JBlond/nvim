6690789ff95333813cdd1d651f167de1d323d0b4
JBlond <leet31337@web.de>
2024-07-03 13:57:32 +0200

    change to english

--- lua/mario/settings.lua
+++ lua/mario/settings.lua
@@ -3,27 +3,27 @@ vim.o.guicursor = 'n-v-c-sm-i-ci-ve:hor20,r-cr-o:hor20,a:blinkwait700-blinkoff40
 -- make the left and right arrow keys change line
 vim.opt.whichwrap:append("<,>,h,l,[,]") 
 
--- Zeilennummern
+-- line numbers
 vim.wo.number = true
 
 vim.opt.listchars = { tab = '»»' ,trail = '·' ,extends = '>', precedes = '<' ,eol = '↲' }
 vim.opt.list = true
 
--- zeilen nicht umbrechen
+-- don't break lines
 vim.wo.wrap = false
 vim.wo.linebreak = false
 
--- Leader Taste setzen
--- Leader = Beliebter Präfix für eigene Tastenkürzel
+-- Set Leader key
+-- Leader = My Prefix for custom key shortcuts
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Tastenkürzel, um :waq einfacher zu machen
+-- key shortcut, to make :waq easier
 --
 -- Parameter 1: Mode (n = normal, i = insert…)
--- Parameter 2: Tastenkürzel (Leader, dann w für „write“)
--- Parameter 3: Andere Tasten
---              Doppelpunkt wa Enter; Doppelpunk q Enter
+-- Parameter 2: key shortcut (Leader, then w for „write“)
+-- Parameter 3: Other keys
+--              Colon wa Enter; colon q Enter
 vim.keymap.set("n", "<leader>q", ":wa<CR> :q<CR>")
 
 -- fast save
@@ -32,7 +32,7 @@ vim.keymap.set("n", "<leader>w", ":wa<CR>")
 -- :help vim.keymap.set
 
 
--- Hilfe-Links mit Enter folgen
+-- Follow Help-Links via Enter
 vim.api.nvim_create_autocmd({"FileType"}, {
   pattern = "help",
   callback = function ()
@@ -41,17 +41,19 @@ vim.api.nvim_create_autocmd({"FileType"}, {
 })
 
 
--- Tastenkürzel mit Callback
+-- key shortcuts with Callback
 vim.keymap.set("n", "<leader>n", function ()
     vim.wo.number = not vim.wo.number
     vim.wo.relativenumber = not vim.wo.relativenumber
 end)
 
 
--- Farbschema
--- vim.o.background = 'light'
+-- color schema
 vim.o.background = 'dark'
+-- vim.o.background = 'light'
 
+vim.opt.termguicolors = true 
+-- Enable GUI colors for the terminal to get truecolor
 
 -- Einrückung mit 4 Spaces
 vim.o.tabstop = 4
@@ -60,22 +62,22 @@ vim.o.shiftwidth = 4
 vim.o.expandtab = true
 
 
--- Aktuelle Einrückung fortführen
+-- resume indent
 vim.o.smartindent = true
 
 
--- Undo-Historie abspeichern
+-- save Undo-Historie
 vim.opt.undofile = true
 
 
--- System-Clipboard verwenden
+-- use System-Clipboard
 vim.o.clipboard = 'unnamedplus'
 
 
--- Beim Wiederöffnen einer Datei zur letzten Position springen
+-- go to the last postion when re-open a file
 vim.api.nvim_command([[au BufReadPost * if line("'\"") > 0 && line("'\"") <= line("$") | exe "normal! g`\"" | endif]])
 
 
--- netrw ausschalten
+-- disable netrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
