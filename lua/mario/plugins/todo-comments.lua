fb6be273d4e05dc4606e78ee1921ab96fc7bf3fd
JBlond <leet31337@web.de>
2024-12-30 08:34:52 +0100

    Change icons

--- lua/mario/plugins/todo-comments.lua
+++ lua/mario/plugins/todo-comments.lua
@@ -1,5 +1,11 @@
 return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {}
+    opts = {
+        signs = true,
+        keywords = {
+            TODO = { icon = "ℹ️ ", color = "info" },
+            WARN = { icon = "❌ ", color = "warning", alt = { "WARNING", "XXX" } },
+        }
+    }
 }
