7943b7735b7c2ca8982b57ff549bc55fcc7bb1f3
JBlond <leet31337@web.de>
2024-12-30 08:38:40 +0100

    remove options

--- lua/mario/plugins/todo-comments.lua
+++ lua/mario/plugins/todo-comments.lua
@@ -1,11 +1,5 @@
 return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {
-        signs = true,
-        keywords = {
-            TODO = { icon = "ℹ️ ", color = "info" },
-            WARN = { icon = "❌ ", color = "warning", alt = { "WARNING", "XXX" } },
-        }
-    }
+    opts = {}
 }
