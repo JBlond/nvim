26d92dbfad23f2f881935c7decc80f1671cd2db5
JBlond <leet31337@web.de>
2025-01-02 21:48:01 +0100

    Add UTF-8 icons

--- lua/mario/plugins/todo-comments.lua
+++ lua/mario/plugins/todo-comments.lua
@@ -1,5 +1,18 @@
 return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {}
+    opts = {
+        highlight = {
+            comments_only = false,
+        },
+        keywords = {
+            BUG  = { icon = "🐛", color = "error", alt = { "BROKEN", "FIXME", "ISSUE" } },
+            HACK = { icon = "🔥", color = "warning" },
+            IDEA = { icon = "💡", color = "test" },
+            NOTE = { icon = "ℹ️", color = "hint", alt = { "INFO" } },
+            TEST = { icon = "🧪", color = "test", alt = { "EXPERIMENT", "TESTING" } },
+            TODO = { icon = "✅", color = "info" },
+            WARN = { icon = "⚠️", color = "warning", alt = { "WARNING", "XXX" } },
+        },
+    },
 }
