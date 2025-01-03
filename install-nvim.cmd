d5a380702f36b12d9f48e3c5a467e6d6857ea751
JBlond <leet31337@web.de>
2025-01-03 09:58:49 +0100

    Add Windows install script

---
+++ install-nvim.cmd
@@ -0,0 +1,8 @@
+@echo off
+winget install --id Microsoft.Powershell --source winget
+winget install --id "Neovim.Neovim"
+winget install python3
+winget install sharkdp.fd
+winget install "Lua for Windows"
+winget install DEVCOM.LuaJIT
+echo "Install RipGrep from https://github.com/BurntSushi/ripgrep/releases"
