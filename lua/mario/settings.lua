083d4a5d13d4fc772ceeee3548850dc04ffb88ea
JBlond <leet31337@web.de>
2025-01-03 21:20:33 +0100

    Update alignment

--- lua/mario/settings.lua
+++ lua/mario/settings.lua
@@ -4,14 +4,15 @@ vim.g.loaded_perl_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 
-vim.opt.termguicolors = true                       -- use all colors
-vim.opt.guicursor = 'a:hor20'                      -- set the cursor
-vim.opt.cursorline = true                          -- Show which line your cursor is on
-vim.opt.inccommand = 'split'                       -- Preview substitutions live, as you type!
+vim.opt.termguicolors = true                         -- use all colors
+vim.opt.guicursor = 'a:hor20'                        -- set the cursor
+vim.opt.cursorline = true                            -- Show which line your cursor is on
+-- vim.opt.cursorcolumn = true                       -- highlight the current column
+vim.opt.inccommand = 'split'                         -- Preview substitutions live, as you type!
 
-vim.opt.hlsearch = true                            -- Set highlight on search,
+vim.opt.hlsearch = true                              -- Set highlight on search,
 
-vim.wo.number = true                               -- Show line numbers
+vim.wo.number = true                                 -- Show line numbers
 vim.opt.listchars = {
     tab = '»»' ,
     trail = '·' ,
@@ -20,26 +21,26 @@ vim.opt.listchars = {
     eol = '↲',
     nbsp = '␣'
 }
-vim.opt.list = true                                 -- show "hidden" list characters
-vim.wo.wrap = false                                 -- don't break lines
-vim.wo.linebreak = false                            -- don't break lines
-vim.opt.whichwrap:append("<,>,h,l,[,]")             -- make the left and right arrow keys change line
+vim.opt.list = true                                  -- show "hidden" list characters
+vim.wo.wrap = false                                  -- don't break lines
+vim.wo.linebreak = false                             -- don't break lines
+vim.opt.whichwrap:append("<,>,h,l,[,]")              -- make the left and right arrow keys change line
 
-vim.api.nvim_create_autocmd({"VimLeave"}, {         -- fix wrong cursor when leaving tmux
+vim.api.nvim_create_autocmd({"VimLeave"}, {          -- fix wrong cursor when leaving tmux
     callback = function ()
         vim.cmd('nvim_cursor_set_shape("horizontal-bar")')
     end
 })
 
-vim.api.nvim_create_autocmd({"FileType"}, {         -- Follow Help-Links via Enter
+vim.api.nvim_create_autocmd({"FileType"}, {          -- Follow Help-Links via Enter
     pattern = "help",
     callback = function ()
         vim.keymap.set("n", "<CR>", "<C-]>", {buffer = true})
     end
 })
 
-vim.opt.signcolumn = yes                            -- show column before number for ToDo Plugin
-vim.opt.conceallevel = 0                            -- show `` in markdown files
+vim.opt.signcolumn = yes                             -- show column before number for ToDo Plugin
+vim.opt.conceallevel = 0                             -- show `` in markdown files
 
 -- indent with 4 Spaces
 vim.opt.tabstop = 4
