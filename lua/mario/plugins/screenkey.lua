20842ed7ccfbf47b1e2e76a0ce3d60578b106737
JBlond <leet31337@web.de>
2025-01-04 20:42:31 +0100

    Override not visible keys

--- lua/mario/plugins/screenkey.lua
+++ lua/mario/plugins/screenkey.lua
@@ -2,4 +2,32 @@ return {
     "NStefan002/screenkey.nvim",
     lazy = false,
     version = "*", -- or branch = "dev", to use the latest commit
+    config = function()
+        require("screenkey").setup {
+            keys = {
+                ["<TAB>"] = "Tab",
+                ["<CR>"] = "return",
+                ["<SPACE>"] = "Space",
+                ["<BS>"] = "<-",
+                ["<DEL>"] = "Del",
+                ["<LEFT>"] = "Left",
+                ["<RIGHT>"] = "Right",
+                ["<UP>"] = "Up",
+                ["<DOWN>"] = "Down",
+                ["<F1>"] = "F1",
+                ["<F2>"] = "F2",
+                ["<F3>"] = "F3",
+                ["<F4>"] = "F4",
+                ["<F5>"] = "F5",
+                ["<F6>"] = "F6",
+                ["<F7>"] = "F7",
+                ["<F8>"] = "F8",
+                ["<F9>"] = "F9",
+                ["<F10>"] = "F10",
+                ["<F11>"] = "F11",
+                ["<F12>"] = "F12",
+                ["SUPER"] = "Super",
+            },
+        }
+    end
 }
