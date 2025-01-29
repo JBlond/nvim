1f834158eced233f36226b3e182e79ea29955045
JBlond <leet31337@web.de>
2025-01-29 16:06:43 +0100

    correct path

--- install-nvim.sh
+++ install-nvim.sh
@@ -5,6 +5,6 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x8
 sudo rm -rf /opt/nvim-linux-x86_64
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 rm nvim-linux-x86_64.tar.gz
-echo -e "Add nvim to your path like export ${RED}PATH=\"\$PATH:/opt/nvim-linux64/bin\"${ENDCOLOR}"
+echo -e "Add nvim to your path like export ${RED}PATH=\"\$PATH:/opt/nvim-linux-x86_64/bin\"${ENDCOLOR}"
 echo -e "Install depenencies ${RED}sudo apt install luarocks fd-find ripgrep${ENDCOLOR}"
 echo -e "Install depenencies ${RED}sudo nala install luarocks fd-find ripgrep${ENDCOLOR}"
