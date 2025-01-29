ce67c1b99516f9e5c697939fb6a7919a74cffb08
JBlond <leet31337@web.de>
2025-01-29 15:47:57 +0100

    Update download path

--- install-nvim.sh
+++ install-nvim.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 RED="\e[31m"
 ENDCOLOR="\e[0m"
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
-rm nvim-linux64.tar.gz
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+rm nvim-linux-x86_64.tar.gz
 echo -e "Add nvim to your path like export ${RED}PATH=\"\$PATH:/opt/nvim-linux64/bin\"${ENDCOLOR}"
 echo -e "Install depenencies ${RED}sudo apt install luarocks fd-find ripgrep${ENDCOLOR}"
 echo -e "Install depenencies ${RED}sudo nala install luarocks fd-find ripgrep${ENDCOLOR}"
