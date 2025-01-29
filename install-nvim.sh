710947056682d32accde6cdf3123f5814ce5ec81
JBlond <leet31337@web.de>
2025-01-29 15:54:40 +0100

    update path

--- install-nvim.sh
+++ install-nvim.sh
@@ -2,7 +2,7 @@
 RED="\e[31m"
 ENDCOLOR="\e[0m"
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-sudo rm -rf /opt/nvim
+sudo rm -rf /opt/nvim-linux-x86_64
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 rm nvim-linux-x86_64.tar.gz
 echo -e "Add nvim to your path like export ${RED}PATH=\"\$PATH:/opt/nvim-linux64/bin\"${ENDCOLOR}"
