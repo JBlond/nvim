332193c212c0bac64953d94e76731ae9ff961ff0
JBlond <leet31337@web.de>
2025-01-30 10:32:50 +0100

    Update colors

--- install-nvim.sh
+++ install-nvim.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-RED="\e[31m"
+RED="\e[1;41m"
 ENDCOLOR="\e[0m"
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim-linux-x86_64
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 rm nvim-linux-x86_64.tar.gz
 echo -e "Add nvim to your path like export ${RED}PATH=\"\$PATH:/opt/nvim-linux-x86_64/bin\"${ENDCOLOR}"
-echo -e "Install depenencies ${RED}sudo apt install luarocks fd-find ripgrep${ENDCOLOR}"
-echo -e "Install depenencies ${RED}sudo nala install luarocks fd-find ripgrep${ENDCOLOR}"
+echo -e "Install depenencies with apt  ${RED}sudo apt install luarocks fd-find ripgrep${ENDCOLOR}"
+echo -e "Install depenencies with nala ${RED}sudo nala install luarocks fd-find ripgrep${ENDCOLOR}"
