984daa956747b71d5edbf1407c4cad807a36723e
JBlond <leet31337@web.de>
2025-02-12 09:16:05 +0100

    switch to scoop

--- install-neovide.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-scoop bucket add extras
-scoop install neovide
--- install-nvim.cmd
+++ install-nvim.cmd
@@ -1,8 +1,8 @@
 @echo off
-winget install --id Microsoft.Powershell --source winget
-winget install --id "Neovim.Neovim"
-winget install python3
-winget install sharkdp.fd
-winget install "Lua for Windows"
-winget install DEVCOM.LuaJIT
-echo "Install RipGrep from https://github.com/BurntSushi/ripgrep/releases"
+scoop bucket add extras
+scoop install main/lua-for-windows
+scoop install main/luajit
+scoop install main/fd
+scoop install main/ripgrep
+scoop install main/neovim
+scoop install neovide
