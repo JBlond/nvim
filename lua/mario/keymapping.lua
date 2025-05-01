52813be34f13ba87f97097c8b54f2b1175b9f175
JBlond <leet31337@web.de>
2025-05-01 10:10:45 +0200

    Formatting

--- lua/mario/keymapping.lua
+++ lua/mario/keymapping.lua
@@ -1,37 +1,40 @@
-vim.g.mapleader = ' '                                                                         -- Set Leader key SPACE
-vim.g.maplocalleader = ' '                                                                    -- Leader = My Prefix for custom key shortcuts
-                                                                                              --
-                                                                                              -- Parameter 1: Mode (n = normal, i = insert…)
-                                                                                              -- Parameter 2: key shortcut (Leader, then w for „write“)
-                                                                                              -- Parameter 3: Other keys
-                                                                                              --              Colon wa Enter; colon q Enter
-vim.keymap.set("n", "<leader>q", ":wa<CR> :q<CR>", { desc = 'save and [q]uit'})               -- fast save && quit
-vim.keymap.set("n", "<leader>w", ":wa<CR>", { desc = '[w]rite / save all'})                   -- fast save
-vim.keymap.set("n", "<C-s>", ":wa<CR>", { desc = 'write / [s]ave all' })                      -- fast save
-vim.keymap.set("n", "<C-n>", ":bnext<CR>", { desc = '[n]ext buffer' })                        -- move among buffers with CTRL + n (next)
-                                                                                              -- :help vim.keymap.set
-                                                                                              -- 
-vim.keymap.set("n", "<leader>n",                                                              -- key shortcuts with Callback
+vim.g.mapleader = ' '                                                           -- Set Leader key SPACE
+vim.g.maplocalleader = ' '                                                      -- Leader = My Prefix for custom key shortcuts
+                                                                                --
+                                                                                -- Parameter 1: Mode (n = normal, i = insert…)
+                                                                                -- Parameter 2: key shortcut (Leader, then w for „write“)
+                                                                                -- Parameter 3: Other keys
+                                                                                --              Colon wa Enter; colon q Enter
+vim.keymap.set("n", "<leader>q", ":wa<CR> :q<CR>", { desc = 'save and [q]uit'}) -- fast save && quit
+vim.keymap.set("n", "<leader>w", ":wa<CR>", { desc = '[w]rite / save all'})     -- fast save
+vim.keymap.set("n", "<C-s>", ":wa<CR>", { desc = 'write / [s]ave all' })        -- fast save
+vim.keymap.set("n", "<C-n>", ":bnext<CR>", { desc = '[n]ext buffer' })          -- move among buffers with CTRL + n (next)
+                                                                                -- :help vim.keymap.set
+                                                                                -- 
+vim.keymap.set("n", "<leader>n",                                                -- key shortcuts with Callback
     function()        
         vim.wo.number = not vim.wo.number
         vim.wo.relativenumber = not vim.wo.relativenumber
     end,
     { desc = 'toogle relative [n]umbering' }
 )
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')                                           -- but clear on pressing <Esc> in normal mode
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')                             -- but clear on pressing <Esc> in normal mode
 
 -- Telescope
 vim.keymap.set("n", "<leader>he", ":Telescope help_tags<CR>", { desc = '[he]lp' })            -- Leader he für „help“
 vim.keymap.set("n", "<C-f>", ":Telescope current_buffer_fuzzy_find<CR>", { desc = '[f]ind' }) -- search inside files
 vim.keymap.set("n", "<C-p>", ":Telescope find_files<CR>")                                     -- find files like in sublime and VSCode
 vim.keymap.set("n", "<M-f>", ":Telescope live_grep<CR>", { desc = 'grep [f]ind' })            -- Grep (find grep)
-vim.keymap.set('n', '<leader><leader>', ":Telescope buffers<CR>", { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader><leader>',                                                       -- Find in existing buffers
+    ":Telescope buffers<CR>", 
+    { desc = '[ ] Find existing buffers' }
+)
 
 -- nvim-tree
 vim.keymap.set("n", "<leader>sf", ":NvimTreeToggle<CR>", { desc = '[s]how [f]iles'})          -- sf - show files
 vim.keymap.set("n", "<leader>jf", ":NvimTreeFindFile<CR>zz", { desc = '[j]jump to [f]ile'})   -- jump (to) file, then center (zz)
 
 -- todo-comments
-vim.keymap.set("n", "<M-t>", ":TodoTelescope<CR>", { desc = 'toggle [t]odo'}) -- toggle Todo
+vim.keymap.set("n", "<M-t>", ":TodoTelescope<CR>", { desc = 'toggle [t]odo'})                  -- toggle Todo
 
-vim.keymap.set("n", "<C-k>", ":Screenkey toggle<CR>" , { desc = 'toggle Scren [k]ey'}) -- toggle Screenkey
+vim.keymap.set("n", "<C-k>", ":Screenkey toggle<CR>" , { desc = 'toggle Scren [k]ey'})         -- toggle Screenkey
