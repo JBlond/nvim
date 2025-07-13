aa2d91f5f56a8c178d837920cb0f8a83394559d0
JBlond <leet31337@web.de>
2025-07-13 17:08:11 +0200

    update

--- lua/mario/plugins/telescope.lua
+++ lua/mario/plugins/telescope.lua
@@ -1,7 +1,10 @@
 return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
-    dependencies = {"nvim-lua/plenary.nvim"},
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope.nvim"
+    },
     config = function()
         require("telescope").setup {
             file_ignore_patterns = {".git/", "./node_modules/*", "node_modules", "^node_modules/*", "node_modules/*"}
