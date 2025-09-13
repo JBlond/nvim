0ad04dcd7ba9dd253528f289dd88f16844b22edd
JBlond <leet31337@web.de>
2025-09-13 10:28:31 +0200

    optimize loading

--- lua/mario/autocmd.lua
+++ lua/mario/autocmd.lua
@@ -1,18 +1,21 @@
-vim.api.nvim_create_autocmd({"VimLeave"}, {          -- fix wrong cursor when leaving tmux
-    callback = function ()
-        vim.cmd('nvim_cursor_set_shape("horizontal-bar")')
-    end
-})
-
-vim.api.nvim_create_autocmd({"FileType"}, {          -- Follow Help-Links via Enter
-    pattern = "help",
-    callback = function ()
-        vim.keymap.set("n", "<CR>", "<C-]>", {buffer = true})
-    end
-})
-
-vim.api.nvim_create_autocmd("TextYankPost", {        -- Highlight yanked text
+-- fix wrong cursor when leaving tmux
+vim.api.nvim_create_autocmd("VimLeave", {
     callback = function()
-        vim.highlight.on_yank({ higroup = "IncSearch", timeout = 500 })
+    vim.fn.system('nvim_cursor_set_shape("horizontal-bar")')
+    end,
+})
+
+-- Follow Help-Links via Enter
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "help",
+    callback = function()
+    vim.keymap.set("n", "<CR>", "<C-]>", { buffer = true, silent = true })
+    end,
+})
+
+-- Highlight yanked text
+vim.api.nvim_create_autocmd("TextYankPost", {
+    callback = function()
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 300 })
     end,
 })
