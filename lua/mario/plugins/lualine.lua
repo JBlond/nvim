39c28c432f4c101bbc437ce35541520a8402a9d7
JBlond <leet31337@web.de>
2025-09-13 10:11:00 +0200

    Show total lines

--- lua/mario/plugins/lualine.lua
+++ lua/mario/plugins/lualine.lua
@@ -2,21 +2,29 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = {"kyazdani42/nvim-web-devicons"},
     config = function()
-        require("lualine").setup {
-            sections = {
-                lualine_z = {
-                    {
-                        function()
-                            for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-                                if vim.api.nvim_buf_get_option(buf, "modified") then
-                                    return "Unsaved buffers" -- any message or icon
-                                end
+    require("lualine").setup {
+        sections = {
+            lualine_z = {
+                {
+                    function()
+                        for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+                            if vim.api.nvim_buf_get_option(buf, "modified") then
+                                    return "Unsaved buffers"
+
                             end
-                            return ""
                         end
-                    }
+                        return ""
+                    end
+                },
+                {
+                    function()
+                    local current_line = vim.fn.line(".")
+                    local total_lines = vim.fn.line("$")
+                    return string.format("%d:%d", current_line, total_lines)
+                    end
                 }
             }
         }
+    }
     end
 }
