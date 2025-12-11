e199c2b4cd86f0022a2284c804289373d2657225
JBlond <leet31337@web.de>
2025-12-11 11:49:40 +0100

    Fix: Show messages only if available

--- install-nvim.sh
+++ install-nvim.sh
@@ -6,9 +6,30 @@ if [ -f nvim-linux-x86_64.tar.gz ]; then
     sudo rm -rf /opt/nvim-linux-x86_64
     sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
     rm nvim-linux-x86_64.tar.gz
-    echo -e "Add nvim to your path like export ${RED}PATH=\"\$PATH:/opt/nvim-linux-x86_64/bin\"${ENDCOLOR}"
-    echo -e "Install depenencies with apt  ${RED}sudo apt install luarocks fd-find ripgrep${ENDCOLOR}"
-    echo -e "Install depenencies with nala ${RED}sudo nala install luarocks fd-find ripgrep${ENDCOLOR}"
+	# Only show PATH hint if /opt/nvim-linux-x86_64/bin is not already in PATH
+	if ! echo "$PATH" | /bin/grep -Eq '(^|:)/opt/nvim-linux-x86_64/bin($|:)'; then
+		echo -e "Added nvim to your path like ${RED}PATH=\"\$PATH:/opt/nvim-linux-x86_64/bin\"${ENDCOLOR}"
+	fi
+
+	# Only show install instructions if any of the required tools are missing
+	missing=()
+	command_exists() { command -v "$1" >/dev/null 2>&1; }
+
+	if ! command_exists luarocks; then
+		missing+=("luarocks")
+	fi
+	# fd can be provided as 'fd' or 'fdfind' depending on distro
+	if ! command_exists fd && ! command_exists fdfind; then
+		missing+=("fd-find")
+	fi
+	if ! command_exists rg; then
+		missing+=("ripgrep")
+	fi
+
+	if [ ${#missing[@]} -gt 0 ]; then
+		echo -e "Install dependencies with apt  ${RED}sudo apt install ${missing[*]}${ENDCOLOR}"
+		echo -e "Install dependencies with nala ${RED}sudo nala install ${missing[*]}${ENDCOLOR}"
+	fi
 else
     echo "Download failed"
 fi
