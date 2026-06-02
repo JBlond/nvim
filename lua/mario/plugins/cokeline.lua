4a5e32f011289ac3957cb828dbba60dd42cd0cfb
JBlond <leet31337@web.de>
2026-06-02 06:29:11 +0200

    Remove additional plugin

--- lua/mario/plugins/cokeline.lua
+++ lua/mario/plugins/cokeline.lua
@@ -3,8 +3,8 @@ return {
     event = "VeryLazy",
     dependencies = {
         "nvim-lua/plenary.nvim",       -- Required for v0.4.0+
-        "nvim-tree/nvim-web-devicons", -- If you want devicons
-        "stevearc/resession.nvim"      -- Optional, for persistent history
+        "nvim-tree/nvim-web-devicons"  -- If you want devicons
+        -- "stevearc/resession.nvim"   -- Optional, for persistent history
     },
     config = function()
         local get_hex = require('cokeline.hlgroups').get_hl_attr
