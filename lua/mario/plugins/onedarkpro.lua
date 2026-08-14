2a630147504958176c01d9eb1c18b26cd5ab45e9
JBlond <leet31337@web.de>
2026-08-14 21:37:55 +0200

    Add theme toggle

--- lua/mario/plugins/onedarkpro.lua
+++ lua/mario/plugins/onedarkpro.lua
@@ -3,5 +3,12 @@ return {
     priority = 1000, -- Ensure it loads first
     config = function()
         vim.cmd.colorscheme("onedark")
+        vim.keymap.set("n", "<leader>tl", function()
+            if vim.g.colors_name == "onedark" then
+                vim.cmd.colorscheme("onedark_dark")
+            else
+                vim.cmd.colorscheme("onedark")
+            end
+        end, { desc = "Toggle light/dark colorscheme" })
     end
 }
