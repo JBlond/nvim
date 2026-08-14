60e60278cc24ef1e1050f110f52a1750faf277be
JBlond <leet31337@web.de>
2026-08-14 11:28:44 +0200

    Fix: Missing icons in which key

--- lua/mario/plugins/whichkey.lua
+++ lua/mario/plugins/whichkey.lua
@@ -5,5 +5,33 @@ return {
         vim.o.timeout = true
         vim.o.timeoutlen = 300
     end,
-    opts = {}
+    opts = {
+        icons   = { 
+            mappings = false, 
+            breadcrumb = "", 
+            separator = " ", 
+            group = "" 
+        },
+        replace = {
+            key  = { function(k) return k end },
+            desc = { },
+        },
+        filter  = function(m) return m.desc and m.desc ~= "" end,
+        plugins = {
+            marks=false, 
+            registers=false, 
+            spelling={
+                enabled=false
+            },
+            presets = {
+                operators=false, 
+                motions=false, 
+                text_objects=false,
+                windows=false, 
+                nav=false, 
+                z=false, 
+                g=false 
+            }
+        }
+    }
 }
