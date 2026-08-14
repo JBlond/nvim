e9a12843df182be05ea49dc28d2f9e74bb93cefb
JBlond <leet31337@web.de>
2026-08-14 21:54:29 +0200

    Add: use telescope to change theme

--- lua/mario/plugins/onedarkpro.lua
+++ lua/mario/plugins/onedarkpro.lua
@@ -13,13 +13,36 @@ return {
         vim.cmd.colorscheme(themes[1])
 
         vim.keymap.set("n", "<leader>tc", function()
-            vim.ui.select(themes, {
-                prompt = "Colorscheme:",
-            }, function(choice)
-                if choice then
-                    vim.cmd.colorscheme(choice)
-                end
-            end)
+            require("telescope.pickers")
+                .new({}, {
+                    prompt_title = "Colorschemes",
+
+                    finder = require("telescope.finders").new_table({
+                        results = themes,
+                    }),
+
+                    sorter = require("telescope.config").values.generic_sorter({}),
+
+                    previewer = false,
+
+                    attach_mappings = function(_, map)
+                        map("i", "<CR>", function(prompt_bufnr)
+                            local action_state = require("telescope.actions.state")
+                            local actions = require("telescope.actions")
+
+                            local selection = action_state.get_selected_entry()
+
+                            if selection then
+                                vim.cmd.colorscheme(selection.value)
+                            end
+
+                            actions.close(prompt_bufnr)
+                        end)
+
+                        return true
+                    end,
+                })
+                :find()
         end, { desc = "Choose colorscheme" })
     end
 }
