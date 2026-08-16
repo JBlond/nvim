1e6a19605783744d69d477be0b979416ff52c54b
JBlond <leet31337@web.de>
2026-08-16 09:47:48 +0200

    Update telescope

--- lua/mario/plugins/telescope.lua
+++ lua/mario/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
     "nvim-telescope/telescope.nvim",
     cmd = "Telescope",
-    tag = "0.1.8",
+    tag = "0.2.2",
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-telescope/telescope.nvim"
